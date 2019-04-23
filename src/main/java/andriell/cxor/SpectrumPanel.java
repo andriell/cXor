4c5173888ee5ddcdbbe921b4e560364d2196a9de
andriell <root.andriell@gmail.com>
2019-04-23 18:26:43 +0300

    Key spectrum

--- src/main/java/andriell/cxor/SpectrumPanel.java
+++ src/main/java/andriell/cxor/SpectrumPanel.java
@@ -7,6 +7,15 @@ public class SpectrumPanel extends JPanel {
     private long spectrum[] = null;
     private int size = 2;
     private JPanel rootPanel;
+    public static Color COLOR_BLACK;
+    public static Color COLOR_RED;
+    public static Color COLOR_GREEN;
+
+    static {
+        COLOR_BLACK = new Color(128,128,128);
+        COLOR_RED = new Color(234,153,153);
+        COLOR_GREEN = new Color(182,215,168);
+    }
 
     public JPanel getRootPanel() {
         return rootPanel;
@@ -34,16 +43,16 @@ public class SpectrumPanel extends JPanel {
     }
 
     protected void paintComponent(Graphics g) {
-        g.setColor(Color.black);
+        g.setColor(COLOR_BLACK);
         g.fillRect(0, 0, 258 * size, 102 * size);
         if (spectrum == null || spectrum.length != 256) {
             g.setColor(Color.white);
             g.fillRect(size, size, 256 * size, 100 * size);
             return;
         }
-        g.setColor(Color.green);
+        g.setColor(COLOR_GREEN);
         g.fillRect(size, size, 256 * size, 100 * size);
-        g.setColor(Color.red);
+        g.setColor(COLOR_RED);
         long spectrumMax = 0;
         for (int i = 0; i < 256; i++) {
             spectrumMax = Math.max(spectrumMax, spectrum[i]);
